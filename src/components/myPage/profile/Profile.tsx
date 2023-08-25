f5e67e0c07627f17ad431cfa62410912fd015ff8
jiapril11 <tadadadacoding@gmail.com>
2023-08-25 15:21:08 +0900

    currentUser 전역 상태관리 추가

--- src/components/myPage/profile/Profile.tsx
+++ src/components/myPage/profile/Profile.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCurrentUser } from '../../../api/supabase/users';
 import useSessionStore, { useModalStore } from '../../../zustand/store';
 import ModifyProfile from '../modifyProfile/ModifyProfile';
 import Modal from '../../common/modal/Modal';
@@ -8,17 +6,14 @@ import { BtnStyleType } from '../../../types/styleTypes';
 import classifyingAge from '../../common/classifyingAge/classifyingAge';
 import defaultImg from '../../../assets/imgs/users/default_profile_img.png';
 import * as St from './style';
+import useCurrentUserStore from '../../../zustand/currentUser';
 
 export default function Profile() {
   const { openedModals, openModal } = useModalStore();
-  const session = useSessionStore((state) => state.session);
-  console.log(session);
-  const userId = session?.user.id;
   const storagaUrl = process.env.REACT_APP_SUPABASE_STORAGE_URL;
+  const currentUser = useCurrentUserStore((state) => state.currentUser);
 
-  const { isLoading, data: currentUser } = useQuery(['currentUser', userId], () => getCurrentUser(userId as string));
-
-  const ageRange = classifyingAge('1999-01-01');
+  const ageRange = classifyingAge(currentUser?.birthday as string);
   return (
     <St.ProfileWrapper>
       <Button styleType={BtnStyleType.BTN_DARK} onClick={() => openModal('modifyProfile')}>
