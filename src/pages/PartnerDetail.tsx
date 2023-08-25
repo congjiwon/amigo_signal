335c5b4844436aa52490fb3c929f114d1d95f54b
heeyeon <godeeyeon@gmail.com>
2023-08-25 12:51:41 +0900

    Feat. 동행 디테일 페이지: 모집상태 및 참여여부 태그 완료

--- src/pages/PartnerDetail.tsx
+++ src/pages/PartnerDetail.tsx
@@ -1,20 +1,40 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
-import { getPartnerPost } from '../api/supabase/partner';
+import { checkApply, getPartnerPost } from '../api/supabase/partner';
 import PartnerCommentsList from '../components/partner/partnerComment/PartnerComments';
 import PartnerDetailInfo from '../components/partner/partnerDetailInfo/PartnerDetailInfo';
 import Communication from '../components/partner/communicate/Communication';
 import useSessionStore from '../zustand/store';
 import ConfirmedPartnerList from '../components/partner/communicate/ConfirmedPartnerList';
 import * as St from './style';
+import { useEffect, useState } from 'react';
 
 function PartnerDetail() {
   const { postid } = useParams<string>();
   const { session } = useSessionStore();
   const logInUserId = session?.user.id;
 
-  const { data: partnerPost, isLoading, isError } = useQuery(['partnerPost', postid], () => getPartnerPost({ postId: postid as string }));
+  const [isApply, setIsApply] = useState<boolean | null>(null);
+  const [isAccepted, setIsAccepted] = useState<boolean | null>();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      if (postid && logInUserId) {
+        const applyHistory = await checkApply(postid, logInUserId);
+        if (applyHistory && applyHistory.length > 0) {
+          setIsApply(true);
+          const AcceptedStatus = applyHistory[0].isAccepted;
+          setIsAccepted(AcceptedStatus);
+        } else {
+          setIsApply(false);
+          setIsAccepted(null);
+        }
+      }
+    };
+    fetchData();
+  }, [postid, logInUserId]);
+
+  const { data: partnerPost, isLoading, isError } = useQuery(['partnerPost', postid], () => getPartnerPost({ postId: postid as string }));
   if (isLoading) {
     return <div>Loading...</div>;
   }
@@ -24,6 +44,7 @@ function PartnerDetail() {
   }
 
   const partnerPostData = partnerPost.data!;
+
   return (
     <>
       <St.PartnerDetailMain>
@@ -34,6 +55,10 @@ function PartnerDetail() {
           {logInUserId ? <Communication postId={postid} writerId={partnerPostData.writerId} logInUserId={logInUserId} /> : <></>}
         </St.CommunicateDiv>
       </St.PartnerDetailMain>
+      <St.Status>
+        <St.PostStatus isOpen={partnerPostData.isOpen}>{partnerPostData.isOpen ? '모집중' : '모집완료'}</St.PostStatus>
+        {isApply && <St.ApplyStatus>{isAccepted === null ? '참여 신청 중' : isAccepted ? '참여 수락됨' : '참여 거절됨'}</St.ApplyStatus>}
+      </St.Status>
       <PartnerCommentsList />
     </>
   );
--- src/pages/style.ts
+++ src/pages/style.ts
@@ -1,5 +1,9 @@
 import { styled } from 'styled-components';
 
+type StatusProps = {
+  isOpen: boolean;
+};
+
 export const PartnerDetailMain = styled.div`
   display: flex;
   justify-content: space-between;
@@ -7,4 +11,32 @@ export const PartnerDetailMain = styled.div`
 
 export const CommunicateDiv = styled.section`
   margin-top: 30px;
+`;
+
+export const Status = styled.div`
+  display: flex;
+  gap: 5px;
+`;
+
+export const PostStatus = styled.span<StatusProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2.5rem;
+  padding: 5px;
+  background-color: ${(props) => (props.isOpen ? '#FF7000' : '#994504')};
+  border-radius: 30px;
+  font-size: 0.7rem;
+  color: ${(props) => (props.isOpen ? '#000000' : '#ffffff')};
+`;
+
+export const ApplyStatus = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  padding: 5px;
+  background-color: #ffb67c;
+  border-radius: 30px;
+  font-size: 0.7rem;
 `;
