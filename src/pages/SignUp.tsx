a386e632e6e4248199082499a4e715693d20d5c5
jiapril11 <tadadadacoding@gmail.com>
2023-08-27 22:06:44 +0900

    Design. 회원가입 UI 적용

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,13 @@
 import SignUpForm from '../components/auth/signUpForm/SignUpForm';
+import Footer from '../components/common/footer/Footer';
+import Header from '../components/common/header/Header';
 
 function SignUp() {
   return (
     <>
+      <Header />
       <SignUpForm />
+      <Footer />
     </>
   );
 }
