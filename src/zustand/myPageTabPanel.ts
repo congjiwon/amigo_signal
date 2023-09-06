7c37659953306f3396dde1a814ba7419d4b8ec9b
jiapril11 <tadadadacoding@gmail.com>
2023-09-06 14:33:55 +0900

    Fix. 글 작성 후 마이페이지에 바로 적용

--- src/zustand/myPageTabPanel.ts
+++ src/zustand/myPageTabPanel.ts
@@ -6,9 +6,8 @@ interface MyPageTabPanel {
 }
 
 const useMyPageTabPanel = create<MyPageTabPanel>((set) => ({
-  // [동행찾기 작성글, 동행찾기 참여글, 동행찾기 북마크, 스팟공유 작성글, 스팟공유 좋아요] 순
-  // [MyPartnerPost, AppliedPosts, ]
-  active: [true, false, false, false, false, false],
+  // [MyPartnerPost, AppliedPosts, BookmarkedPosts, MySpotShare, LikedSpotShare, ModifyProfile]
+  active: [false, false, false, false, false, false],
   setActive: (index, status) =>
     set((state) => {
       const newActive = state.active.map((item) => (item = false));
