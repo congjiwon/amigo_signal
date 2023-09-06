fa0c26877410a270493807402f8c802c7d09ac8f
congjiwon <sheepg1510@gmail.com>
2023-09-06 16:53:56 +0900

    Design.인트로 페이지 디자인 구성 완료

--- src/components/auth/Intro/IntroTemplate.tsx
+++ src/components/auth/Intro/IntroTemplate.tsx
@@ -1,31 +1,43 @@
 import { useNavigate } from 'react-router';
 import * as St from './style';
+import introImg from '../../../assets/imgs/Intro/introImg.png';
+import logo from '../../../assets/imgs/Logo/logo.png';
+import logo_gif from '../../../assets/imgs/Logo/logo_gif.gif';
+
+import { FiMapPin, FiUsers } from 'react-icons/fi';
 
 function IntroTemplate() {
   const navigate = useNavigate();
   return (
-    <St.IntroWrapper>
+    <St.IntroLayout>
+      <St.IntroImgBox>
+        <St.IntroImg src={introImg} alt="Intro" />
+      </St.IntroImgBox>
+
       <St.Container>
-        <St.LogoDiv>
-          <img src={'./logo_bird.png'} alt="amigo-signal" style={{ width: '100%' }} />
-        </St.LogoDiv>
-        <St.Logo>Amigo Signal</St.Logo>
+        {/* <St.LogoDiv> */}
+        <img src={logo_gif} alt="amigo-signal" style={{ width: '170px' }} />
+        {/* </St.LogoDiv> */}
         <St.ExplanationContainer>
           <St.Span>Amigo Signal과 함께 여행에 동행할 친구를 찾아보세요.</St.Span>
           <St.Span>여행이 더 즐거워질 거예요.</St.Span>
         </St.ExplanationContainer>
         <St.LinkContainer>
           <St.LinkBox onClick={() => navigate('/partner')}>
-            <St.LinkIcon></St.LinkIcon>
-            <div>동행 찾기</div>
+            <St.LinkIcon>
+              <FiUsers style={{ width: '36px', height: '36px', color: 'white' }} />
+            </St.LinkIcon>
+            <p>동행 찾기</p>
           </St.LinkBox>
           <St.LinkBox onClick={() => navigate('/spotshare')}>
-            <St.LinkIcon></St.LinkIcon>
-            <div>스팟 공유</div>
+            <St.LinkIcon>
+              <FiMapPin style={{ width: '36px', height: '36px', color: 'white' }} />
+            </St.LinkIcon>
+            <p>스팟 공유</p>
           </St.LinkBox>
         </St.LinkContainer>
       </St.Container>
-    </St.IntroWrapper>
+    </St.IntroLayout>
   );
 }
 
