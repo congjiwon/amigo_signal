7a509861d584817d53399b20b7aaec651f4ba0f6
congjiwon <sheepg1510@gmail.com>
2023-09-07 11:20:41 +0900

    Design.동행 글작성 버튼 수정

--- src/components/partner/partnerList/style.ts
+++ src/components/partner/partnerList/style.ts
@@ -38,6 +38,7 @@ export const FilterWriteBox = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
 `;
 
 export const Grid = styled.div`
