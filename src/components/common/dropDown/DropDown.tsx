4e8bc51ad89fafe8da2e7b9156c92a16969f8311
congjiwon <sheepg1510@gmail.com>
2023-09-14 21:39:51 +0900

    Design.동행수정페이지 반응형

--- src/components/common/dropDown/DropDown.tsx
+++ src/components/common/dropDown/DropDown.tsx
@@ -75,26 +75,26 @@ export function UpdatePartnerDropDown({ partner, setPartner }: UpdatePartnerProp
   };
   const partnerString = partner.toString();
   return (
-    <Space wrap>
-      <Select
-        placeholder="모집인원"
-        defaultValue={partnerString}
-        style={{ width: '100%' }}
-        onChange={handleChange}
-        options={[
-          { value: '1', label: '1명' },
-          { value: '2', label: '2명' },
-          { value: '3', label: '3명' },
-          { value: '4', label: '4명' },
-          { value: '5', label: '5명' },
-          { value: '6', label: '6명' },
-          { value: '7', label: '7명' },
-          { value: '8', label: '8명' },
-          { value: '9', label: '9명' },
-          { value: '10', label: '10명' },
-        ]}
-      />
-    </Space>
+    // <Space wrap>
+    <Select
+      placeholder="모집인원"
+      className="UpdatePartnerDropDown"
+      defaultValue={partnerString}
+      onChange={handleChange}
+      options={[
+        { value: '1', label: '1명' },
+        { value: '2', label: '2명' },
+        { value: '3', label: '3명' },
+        { value: '4', label: '4명' },
+        { value: '5', label: '5명' },
+        { value: '6', label: '6명' },
+        { value: '7', label: '7명' },
+        { value: '8', label: '8명' },
+        { value: '9', label: '9명' },
+        { value: '10', label: '10명' },
+      ]}
+    />
+    // </Space>
   );
 }
 
--- src/components/partner/partnerUpdateTemplate/style.ts
+++ src/components/partner/partnerUpdateTemplate/style.ts
@@ -7,6 +7,9 @@ export const FormContainer = styled.div`
   font-style: normal;
   font-weight: 400;
   line-height: 150%;
+  @media screen and (max-width: 1260px) {
+    padding: 0 20px;
+  }
 `;
 
 export const WriteForm = styled.form`
@@ -19,13 +22,24 @@ export const SelectListBox = styled.div`
   display: flex;
   gap: 24px;
 
-  @media screen and (max-width: 650px) {
+  .UpdateLocationDropDown,
+  .UpdatePartnerCalendar,
+  .UpdatePartnerDropDown {
+    width: 384px;
+  }
+
+  @media screen and (max-width: 682px) {
     align-items: flex-start;
     flex-direction: column;
     gap: 12px;
 
     & :where(.css-dev-only-do-not-override-byeoj0).ant-space-vertical {
       margin-left: 0 !important;
+    }
+    .UpdateLocationDropDown,
+    .UpdatePartnerCalendar,
+    .UpdatePartnerDropDown {
+      width: 335px;
     }
   }
 `;
